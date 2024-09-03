8ba6effa61d1615e65d83faf4a1c56ee915abf8e
Ramzan <ramzand511@gmail.com>
2024-09-03 12:02:01 +0300

    Доработал adding-azkarType

--- app/Http/Requests/AzkarRequest.php
+++ app/Http/Requests/AzkarRequest.php
@@ -31,7 +31,7 @@ class AzkarRequest extends FormRequest
             ],
             'azkar_type_id' => [
                 'required',
-                'integer'
+                'string'
             ]
         ];
     }
